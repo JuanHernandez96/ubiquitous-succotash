8c5c34f4fee1e3b69c986a91dcfe510130a1f715
Juan Hernandez <juanh22@gmail.com>
2021-09-25 17:16:31 -0600

    need help with error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection(
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'Wolfman4001!@',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
